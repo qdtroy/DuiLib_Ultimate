43613bd172d754fdfda5ca482dc9f3d4ca67ded2
Troy <qdtroy@qq.com>
2016-08-23 14:14:18 +0800

    整理代码

--- DuiLib/Utils/DPI.h
+++ DuiLib/Utils/DPI.h
@@ -20,47 +20,39 @@ typedef enum MONITOR_DPI_TYPE {
 #define DPI_ENUMS_DECLARED
 #endif // (DPI_ENUMS_DECLARED)
 
-
 namespace DuiLib
 {
+	class UILIB_API CDPI
+	{
+	public:
+		CDPI(void);
 
+	public:
+		static int GetMainMonitorDPI();
+		static int GetDPIOfMonitor(HMONITOR hMonitor);
+		static int GetDPIOfMonitorNearestToPoint(POINT pt);
 
+	public:
+		PROCESS_DPI_AWARENESS GetDPIAwareness();
+		BOOL SetDPIAwareness(PROCESS_DPI_AWARENESS Awareness);
+		UINT GetDPI();
+		UINT GetScale();
+		void SetScale(UINT uDPI);
+		RECT Scale(RECT rcRect);
+		void Scale(RECT *pRect);
+		POINT Scale(POINT ptPoint);
+		void Scale(POINT *pPoint);
+		SIZE Scale(SIZE szSize);
+		void Scale(SIZE *pSize);
+		int Scale(int iValue);
 
+		int  ScaleBack(int iValue);
+		void ScaleBack(RECT *pRect);
 
-class UILIB_API CDPI
-{
-public:
-	CDPI(void);
-
-	static int GetMainMonitorDPI();
-	static int GetDPIOfMonitor(HMONITOR hMonitor);
-	static int GetDPIOfMonitorNearestToPoint(POINT pt);
-	
-
-	PROCESS_DPI_AWARENESS GetDPIAwareness();
-	BOOL SetDPIAwareness(PROCESS_DPI_AWARENESS Awareness);
-	UINT GetDPI();
-	UINT GetScale();
-	void SetScale(UINT uDPI);
-	RECT Scale(RECT rcRect);
-	void Scale(RECT *pRect);
-	POINT Scale(POINT ptPoint);
-	void Scale(POINT *pPoint);
-	SIZE Scale(SIZE szSize);
-	void Scale(SIZE *pSize);
-	int Scale(int iValue);
-
-
-	int  ScaleBack(int iValue);
-	void ScaleBack(RECT *pRect);
-
-
-private:
+	private:
 		int m_nScaleFactor;
 		int m_nScaleFactorSDA;
 		PROCESS_DPI_AWARENESS m_Awareness;
-	
-};
-
+	};
 }
 #endif //__DPI_H__

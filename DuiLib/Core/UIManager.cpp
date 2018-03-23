ad199ecffa018153bbafea669950158af19e21d6
Troy <qdtroy@qq.com>
2018-03-23 10:13:38 +0800

    修正MapKeyState鼠标按键状态判断错误的问题

--- DuiLib/Core/UIManager.cpp
+++ DuiLib/Core/UIManager.cpp
@@ -29,8 +29,8 @@ namespace DuiLib {
 	{
 		UINT uState = 0;
 		if( ::GetKeyState(VK_CONTROL) < 0 ) uState |= MK_CONTROL;
-		if( ::GetKeyState(VK_RBUTTON) < 0 ) uState |= MK_LBUTTON;
-		if( ::GetKeyState(VK_LBUTTON) < 0 ) uState |= MK_RBUTTON;
+		if( ::GetKeyState(VK_LBUTTON) < 0 ) uState |= MK_LBUTTON;
+		if( ::GetKeyState(VK_RBUTTON) < 0 ) uState |= MK_RBUTTON;
 		if( ::GetKeyState(VK_SHIFT) < 0 ) uState |= MK_SHIFT;
 		if( ::GetKeyState(VK_MENU) < 0 ) uState |= MK_ALT;
 		return uState;

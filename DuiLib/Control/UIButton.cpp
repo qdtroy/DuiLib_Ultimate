5fa45d55b5a36e69a47876e2bb13d52dd36fc267
nakkler <v2x222@163.com>
2022-09-21 16:00:38 +0800

    在按钮上按下左键再按下右键，再松开左键再松开右键，按下按下状态未恢复

--- DuiLib/Control/UIButton.cpp
+++ DuiLib/Control/UIButton.cpp
@@ -79,15 +79,26 @@ namespace DuiLib
 		{
 			if (m_pManager->IsCaptured())
 			{
-				if( (m_uButtonState & UISTATE_CAPTURED) != 0 ) {
-					if( ::PtInRect(&m_rcItem, event.ptMouse) ) m_uButtonState |= UISTATE_PUSHED;
-					else m_uButtonState &= ~UISTATE_PUSHED;
-					Invalidate();
-				}
-			}			
+                if ((m_uButtonState & UISTATE_CAPTURED) != 0)
+                {
+                    if (::PtInRect(&m_rcItem, event.ptMouse))
+                        m_uButtonState |= UISTATE_PUSHED;
+                    else m_uButtonState &= ~UISTATE_PUSHED;
+                    Invalidate();
+                }
+			}
+			else
+			{
+                if ((m_uButtonState & UISTATE_CAPTURED) != 0)
+                {
+                    m_uButtonState &= ~UISTATE_PUSHED;
+                    Invalidate();
+                }
+			}
+
 			return;
 		}
-		if( event.Type == UIEVENT_BUTTONUP )
+		if( event.Type == UIEVENT_BUTTONUP)
 		{
 			if( (m_uButtonState & UISTATE_CAPTURED) != 0 ) {
 				m_uButtonState &= ~(UISTATE_PUSHED | UISTATE_CAPTURED);
